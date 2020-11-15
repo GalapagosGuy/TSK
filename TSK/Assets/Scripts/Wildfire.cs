af10013935bb14e61384de84904086abd3b9fbfa
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-11-15 23:09:06 +0100

    Rest of formulas

--- TSK/Assets/Scripts/Wildfire.cs
+++ TSK/Assets/Scripts/Wildfire.cs
@@ -30,7 +30,11 @@ public class Wildfire : MonoBehaviour
     private float R;
     private float delta_w;
     private float delta_s;
-
+    private float I_R;
+    private float Epsilon;
+    private float Gamma;
+    private float n_m;
+    private float n_s;
 
     // ZMIENNE POMOCNICZE GŁÓWNEGO WZORU
     private float Beta;
@@ -40,7 +44,8 @@ public class Wildfire : MonoBehaviour
     private float W_n;
     private float A;
     private float e;
-    private float Epsilon;
+    private float Gamma_max;
+
 
     private void Start()
     {
@@ -59,7 +64,17 @@ public class Wildfire : MonoBehaviour
         // wzory z głównego wzoru
         delta_w = wind.C * Mathf.Pow(wind.U, wind.B) * Mathf.Pow((Beta/Beta_op), -1f * wind.E);
         delta_s = Mathf.Pow((5.275f * Beta), -0.3f) * Mathf.Pow((Mathf.Tan(ground.groundAngle)), 2f);
+        Gamma_max = Mathf.Pow(ground.surfaceToVolumeRatio, 1.5f) * Mathf.Pow(495f + 0.594f * Mathf.Pow(ground.surfaceToVolumeRatio, 1.5f), -1f);
+        Gamma = Gamma_max * Mathf.Pow((Beta / Beta_op), A) * Mathf.Exp(A * (1 - (Beta / Beta_op)));
+        n_m = 1 - 2.59f * (ground.M_f / MenuController.GroundAngleController.M_x) + 5.11f * Mathf.Pow((ground.M_f / MenuController.GroundAngleController.M_x), 2) - 3.52f * Mathf.Pow((ground.M_f / MenuController.GroundAngleController.M_x), 3);
+        n_s = 0.174f * Mathf.Pow(MenuController.GroundAngleController.S_e, -0.19f);
 
+
+        float nominator = I_R * Epsilon * (1 + delta_w + delta_s);
+        float denominator = ro_b * e * Q_ig;
+
+        // FINAL RESULT
+        R = nominator / denominator;
 
         startingPosition = this.transform.localPosition;
         oldPosition = startingPosition;
