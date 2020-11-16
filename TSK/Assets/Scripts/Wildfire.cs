cd3969299f4e11fbef7bf2c73551d0de81e5f9f8
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-11-16 01:34:20 +0100

    Fix to tanges

--- TSK/Assets/Scripts/Wildfire.cs
+++ TSK/Assets/Scripts/Wildfire.cs
@@ -72,7 +72,7 @@ public class Wildfire : MonoBehaviour
         // WZORY Z GŁÓWNEGO WZORU
         Epsilon = Mathf.Pow((192f + 0.2595f * ground.surfaceToVolumeRatio), -1f) * Mathf.Exp((0.792f + 0.681f * Mathf.Pow(ground.surfaceToVolumeRatio, 0.5f)) * (Beta + 0.1f));
         delta_w = wind.C * Mathf.Pow(wind.U, wind.B) * Mathf.Pow((Beta / Beta_op), -1f * wind.E);
-        delta_s = 5.275f * Mathf.Pow(Beta, -0.3f) * Mathf.Pow((Mathf.Tan(ground.groundAngle)), 2f);
+        delta_s = 5.275f * Mathf.Pow(Beta, -0.3f) * Mathf.Pow((Mathf.Tan(Mathf.Deg2Rad*ground.groundAngle)), 2f);
         Gamma_max = Mathf.Pow(ground.surfaceToVolumeRatio, 1.5f) * Mathf.Pow(495f + 0.0594f * Mathf.Pow(ground.surfaceToVolumeRatio, 1.5f), -1f);
         Gamma = Gamma_max * Mathf.Pow((Beta / Beta_op), A) * Mathf.Exp(A * (1 - (Beta / Beta_op)));
         n_m = 1f - 2.59f * (ground.M_f / MenuController.GroundAngleController.M_x) + 5.11f * Mathf.Pow((ground.M_f / MenuController.GroundAngleController.M_x), 2) - 3.52f * Mathf.Pow((ground.M_f / MenuController.GroundAngleController.M_x), 3);
