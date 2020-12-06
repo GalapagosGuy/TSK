0d3d0630918e69da4ba654451c59b1693fb1ada3
Zawisz <zawisza_mateusz@wp.pl>
2020-12-06 21:52:29 +0100

    Improved ground rotation

---
+++ TSK/Assets/Scripts/CameraFocusPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocusPosition : MonoBehaviour
+{
+    public GameObject objectToFollow = null;
+
+    [SerializeField]
+    private float positionOffsetZ = -4.0f;
+    [SerializeField]
+    private float positionOffsetY = 3.5f;
+    [SerializeField]
+    private float maxAngle = 60.0f;
+    [SerializeField]
+    private float baseZposition = -14.0f;
+    [SerializeField]
+    private float baseYposition = 2.5f;
+
+    private void FixedUpdate()
+    {
+        if (objectToFollow)
+        {
+            float percentageValue = objectToFollow.transform.rotation.eulerAngles.z / maxAngle;
+
+            Vector3 position = this.transform.position;
+            position.z = baseZposition + percentageValue * positionOffsetZ;
+            position.y = baseYposition + percentageValue * positionOffsetY;
+            this.transform.position = position;
+        }
+    }
+}
---
+++ TSK/Assets/Scripts/RightGroundPositionFixer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightGroundPositionFixer : MonoBehaviour
+{
+    public GameObject objectToFollow;
+
+    private void Update()
+    {
+        this.transform.position = objectToFollow.transform.position;
+    }
+}
