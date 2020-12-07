767c9205107d18d85d74174e38dd77555419185b
Zawisz <zawisza_mateusz@wp.pl>
2020-12-07 09:51:08 +0100

    Models on scene depending on fuel type

---
+++ TSK/Assets/Scripts/Enviro.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviro : MonoBehaviour
+{
+    public GameObject[] grass;
+    public GameObject[] chaparral;
+    public GameObject[] timber;
+    public GameObject[] loggingSlash;
+
+    public void ShowGrass()
+    {
+        DisplayModels(grass, true);
+        DisplayModels(chaparral, false);
+        DisplayModels(timber, false);
+        DisplayModels(loggingSlash, false);
+    }
+
+    public void ShowChaparral()
+    {
+        DisplayModels(grass, false);
+        DisplayModels(chaparral, true);
+        DisplayModels(timber, false);
+        DisplayModels(loggingSlash, false);
+    }
+
+    public void ShowTimber()
+    {
+        DisplayModels(grass, false);
+        DisplayModels(chaparral, false);
+        DisplayModels(timber, true);
+        DisplayModels(loggingSlash, false);
+    }
+
+    public void ShowLoggingSlash()
+    {
+        DisplayModels(grass, false);
+        DisplayModels(chaparral, false);
+        DisplayModels(timber, false);
+        DisplayModels(loggingSlash, true);
+    }
+
+    private void DisplayModels(GameObject[] modelsPack, bool value)
+    {
+        foreach (GameObject go in modelsPack)
+            go.SetActive(value);
+    }
+}
--- TSK/Assets/Scripts/FuelTypeController.cs
+++ TSK/Assets/Scripts/FuelTypeController.cs
@@ -9,6 +9,8 @@ public class FuelTypeController : MonoBehaviour
     [SerializeField]
     private Dropdown fuelDropdown;
 
+    public Enviro enviroManager = null;
+
     private MenuController.GroundAngleController ground;
     private Wildfire fire;
     void Start()
@@ -19,22 +21,23 @@ public class FuelTypeController : MonoBehaviour
 
     public void OnValueChanged()
     {
-        if(fuelDropdown.value == 0)
+        if (fuelDropdown.value == 0)
         {
             // grass (short) Fine
-            
+
             ground.surfaceToVolumeRatio = 3500f;
             ground.W_o = 0.034f;
             ground.fuelDepth = 1.0f;
-
+            enviroManager?.ShowGrass();
         }
 
         if (fuelDropdown.value == 1)
         {
             // Chaparral Living
             ground.surfaceToVolumeRatio = 1500f;
-            ground.W_o = 0.23f;          
+            ground.W_o = 0.23f;
             ground.fuelDepth = 6.0f;
+            enviroManager?.ShowChaparral();
         }
 
         if (fuelDropdown.value == 2)
@@ -43,6 +46,7 @@ public class FuelTypeController : MonoBehaviour
             ground.surfaceToVolumeRatio = 1500f;
             ground.W_o = 0.023f;
             ground.fuelDepth = 1.5f;
+            enviroManager?.ShowTimber();
         }
 
         if (fuelDropdown.value == 3)
@@ -51,6 +55,7 @@ public class FuelTypeController : MonoBehaviour
             ground.surfaceToVolumeRatio = 109f;
             ground.W_o = 0.644f;
             ground.fuelDepth = 2.3f;
+            enviroManager?.ShowLoggingSlash();
         }
 
         fire.Recalculate();
